8caa76f642856d4224fbdca29ece46b3010cdf1f
Krixx <dev@krixx.anonaddy.com>
2022-12-05 18:29:23 +0100

    Solved day4

---
+++ AdventOfCode/Day4.cs
@@ -0,0 +1,74 @@
+﻿namespace AdventOfCode
+{
+    internal class Day4
+    {
+        private readonly string _filename = $"{AppDomain.CurrentDomain.BaseDirectory}\\Inputs\\day4.txt";
+
+        public Day4()
+        {
+            Console.WriteLine("--- Day 4 ---");
+
+            Part1();
+            Part2();
+        }
+
+        private void Part1()
+        {
+            var totalOverlaps = 0;
+            var lines = File.ReadLines(_filename);
+            foreach (var line in lines)
+            {
+                var pairs = line.Split(",");
+
+                var section1 = new Section(pairs[0].Split("-"));
+                var section2 = new Section(pairs[1].Split("-"));
+
+                var smallerCount = section1.Range.Count < section2.Range.Count
+                    ? section1.Range.Count
+                    : section2.Range.Count;
+
+                var overlaps = section1.Range.Intersect(section2.Range).Count() == smallerCount;
+                if (overlaps)
+                    totalOverlaps++;
+            }
+
+            Console.WriteLine("[Part 1] " + totalOverlaps);
+        }
+
+        private void Part2()
+        {
+            var totalOverlaps = 0;
+            var lines = File.ReadLines(_filename);
+            foreach (var line in lines)
+            {
+                var pairs = line.Split(",");
+
+                var section1 = new Section(pairs[0].Split("-"));
+                var section2 = new Section(pairs[1].Split("-"));
+
+                var overlaps = section1.Range.Intersect(section2.Range).Any();
+                if (overlaps)
+                    totalOverlaps++;
+            }
+
+            Console.WriteLine("[Part 2] " + totalOverlaps);
+        }
+    }
+
+    public class Section
+    {
+        public Section(string[] section)
+        {
+            var start = int.Parse(section[0]);
+            var finish = int.Parse(section[1]);
+
+            Range = new List<int>();
+            for (var i = start; i <= finish; i++)
+            {
+                Range.Add(i);
+            }
+        }
+
+        public List<int> Range { get; set; }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,8 @@
         {
             //var day1 = new Day1();
             //var day2 = new Day2();
-            var day3 = new Day3();
+            //var day3 = new Day3();
+            var day4 = new Day4();
         }
     }
 }
